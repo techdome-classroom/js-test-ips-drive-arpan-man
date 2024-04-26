5e371f02270f1378ba2b1e1c1bdc738ffa72bd15
arpan-man <125387458+arpan-man@users.noreply.github.com>
2024-04-26 13:40:51 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,27 +1,27 @@
 function smallestMissingPositiveInteger(nums) {
   const n = nums.length;
 
-  let pind = 0;
+  let posind = 0;
   for (let i = 0; i < n; i++) {
       if (nums[i] > 0) {
-          [nums[i], nums[pind]] = [nums[pind], nums[i]];
-          pind++;
+          [nums[i], nums[posind]] = [nums[posind], nums[i]];
+          posind++;
       }
   }
 
-  for (let i = 0; i < pind; i++) {
+  for (let i = 0; i < posind; i++) {
       const val = Math.abs(nums[i]);
-      if (val - 1 < pind && nums[val - 1] > 0) {
+      if (val - 1 < posind && nums[val - 1] > 0) {
           nums[val - 1] = -nums[val - 1];
       }
   }
 
-  for (let i = 0; i < pind; i++) {
+  for (let i = 0; i < posind; i++) {
       if (nums[i] > 0) {
           return i + 1;
       }
   }
-  return pind + 1;
+  return posind + 1;
 }
   module.exports = smallestMissingPositiveInteger;
 
